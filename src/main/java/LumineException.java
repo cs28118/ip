a9538c237f5a218b795d306eded06bb34aeff5d6
cs28118 <limcs5784@gmail.com>
2026-08-20 12:53:12 +0800

    LumineException.java: create new exception class

---
+++ src/main/java/LumineException.java
@@ -0,0 +1,10 @@
+/**
+ * Represents an input error that can be explained to the user.
+ */
+public class LumineException extends RuntimeException {
+
+    public LumineException(String message) {
+        super(message);
+    }
+
+}
