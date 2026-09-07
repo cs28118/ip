cdcf01177a584678435b629e57346f9a5647ddf0
cs28118 <limcs5784@gmail.com>
2026-09-07 14:02:50 +0800

    Update user and lumine image

--- src/main/java/lumine/ui/MainWindow.java
+++ src/main/java/lumine/ui/MainWindow.java
@@ -30,7 +30,7 @@ public class MainWindow extends AnchorPane {
     private final Image userImage = new Image(Objects.requireNonNull(
             MainWindow.class.getResource("/images/user.gif")).toExternalForm());
     private final Image lumineImage = new Image(Objects.requireNonNull(
-            MainWindow.class.getResource("/images/lumine.jpg")).toExternalForm());
+            MainWindow.class.getResource("/images/lumine_profile.jpg")).toExternalForm());
 
     @FXML
     private void initialize() {
