1e9e86981db52ee12836ed305ace7dab51ce10b3
cs28118 <limcs5784@gmail.com>
2026-09-14 12:38:18 +0800

    Change chatbot profile picture

--- src/main/java/lumine/ui/MainWindow.java
+++ src/main/java/lumine/ui/MainWindow.java
@@ -29,7 +29,7 @@ public class MainWindow extends AnchorPane {
     private final Image userImage = new Image(Objects.requireNonNull(
             MainWindow.class.getResource("/images/user.gif")).toExternalForm());
     private final Image lumineImage = new Image(Objects.requireNonNull(
-            MainWindow.class.getResource("/images/lumine_profile.jpg")).toExternalForm());
+            MainWindow.class.getResource("/images/lumine_profile.png")).toExternalForm());
 
     @FXML
     private void initialize() {
